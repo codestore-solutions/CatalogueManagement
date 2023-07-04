9b3b1186eb08025e2ea85042baf75e7e971b8c5f
mayankAwasthi23415 <mayank.a@codestoresolutions.com>
2023-07-04 18:18:09 +0530

    Update in Intercepter
    Token update

--- CatalogueManagement/src/app/services/token.intercepter.ts
+++ CatalogueManagement/src/app/services/token.intercepter.ts
@@ -10,9 +10,9 @@ export class TokenIntercepter implements HttpInterceptor {
 
     }
     intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-        const token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJodHRwOi8vc2NoZW1hcy54bWxzb2FwLm9yZy93cy8yMDA1LzA1L2lkZW50aXR5L2NsYWltcy9uYW1laWRlbnRpZmllciI6ImFkbWluIiwiaHR0cDovL3NjaGVtYXMubWljcm9zb2Z0LmNvbS93cy8yMDA4LzA2L2lkZW50aXR5L2NsYWltcy9yb2xlIjoiYWRtaW4iLCJ0ZW5hbnRJZCI6IlQyIiwiZXhwIjoxNjkxMTM2Nzc2LCJpc3MiOiJodHRwczovL2xvY2FsaG9zdDo3MTM3IiwiYXVkIjoiaHR0cHM6Ly9sb2NhbGhvc3Q6NzEzNyJ9.-2LQXkF9ha9iLT6G_qaFwLkgfNIS-Zd7JezHcMCD-Ng';
+        const token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJodHRwOi8vc2NoZW1hcy54bWxzb2FwLm9yZy93cy8yMDA1LzA1L2lkZW50aXR5L2NsYWltcy9uYW1laWRlbnRpZmllciI6ImFkbWluIiwiaHR0cDovL3NjaGVtYXMubWljcm9zb2Z0LmNvbS93cy8yMDA4LzA2L2lkZW50aXR5L2NsYWltcy9yb2xlIjoiYWRtaW4iLCJ0ZW5hbnRJZCI6IlQxIiwiZXhwIjoxNjkxMTM2NTkwLCJpc3MiOiJodHRwczovL2xvY2FsaG9zdDo3MTM3IiwiYXVkIjoiaHR0cHM6Ly9sb2NhbGhvc3Q6NzEzNyJ9.mcNjnnKYGjq7iqbLgUQ8bwKQfp-oZ7dpFnFVrgChniQ';
         req = req.clone({
-            setHeaders : {
+            setHeaders: {
                 'Authorization': `Bearer ${token}`,
                 'Access-Control-Allow-Origin': '*'
             }
